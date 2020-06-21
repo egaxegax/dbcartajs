85351309e849f21b8fa0c493cae80ba9729d78c9
egax <egax@bk.ru>
2020-06-21 15:21:05 +0300

    +yandex metric

---
+++ js/utils/metrics.js
@@ -0,0 +1,14 @@
+//
+// Add metrics, analytics codes
+//
+// Yandex.Metrika counter
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(65044687, "init", {
+clickmap:true,
+trackLinks:true,
+accurateTrackBounce:true
+});
+// /Yandex.Metrika counter
