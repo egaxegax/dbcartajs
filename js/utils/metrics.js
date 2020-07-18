f512eb1e79b4ee04038d7c874749494818b3a16b
egax <egax@bk.ru>
2020-07-18 17:04:42 +0300

    *metrics *index

--- js/utils/metrics.js
+++ js/utils/metrics.js
@@ -1,25 +1,36 @@
 //
 // Add metrics, analytics codes
 //
-// Yandex.Metrika counter
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+function ifLocal(){
+  return !navigator.onLine || String(window.location).match(/file:|localhost/);
+}
+function addYaMetrics(){
+  // Yandex.Metrika counter
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-ym(65044687, "init", {
-clickmap:true,
-trackLinks:true,
-accurateTrackBounce:true
-});
-// /Yandex.Metrika counter
-// Global site tag (gtag.js) - Google Analytics
-let script = document.createElement('script');
-script.type = 'text/javascript';
-script.src  = 'https://www.googletagmanager.com/gtag/js?id=UA-25857345-4';
-document.head.appendChild(script);
-// ---------
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-25857345-4');
-// /Google Analytics
+  ym(65044687, "init", {
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true
+  });
+  console.log('+ym');
+}
+function addGoogleMetrics(){
+  // Google Analytics
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src  = 'https://www.googletagmanager.com/gtag/js?id=UA-25857345-4';
+  document.head.appendChild(script);
+  // ---------
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-25857345-4');
+  console.log('+gm');
+}
+if(! ifLocal()){
+  addYaMetrics();
+  addGoogleMetrics();
+}
