66677c49ec174cd8e55b967268b33cf260f04c54
agena <agena@agena>
2020-09-05 17:06:25 +0300

    *index.html js <-> html tags

--- js/utils/metrics.js
+++ js/utils/metrics.js
@@ -1,7 +1,9 @@
 //
 // Add metrics, analytics codes
 //
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://egaxegax.github.io/static/js/metrics.js';
-document.head.appendChild(script);
+if(!String(window.location).match(/file:|localhost/)){
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://egaxegax.github.io/static/js/metrics.js';
+  document.head.appendChild(script);
+}
