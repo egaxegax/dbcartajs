2ba5fc5b7b9e528cd3d33cdfe383ecccff375f05
agena <agena@agena>
2021-04-17 21:23:02 +0300

    *mosmeto_info update; +svg animatemotion demo

--- js/data/imgmap.js
+++ js/data/imgmap.js
@@ -2,7 +2,7 @@
 // Image data urls.
 //
 var IMGMAP={
-'wrld_small': 'img/wrld-small.jpg',
+'wrld_small':      'img/wrld-small.jpg',
 'wrld_small_merc': 'img/wrld-small-merc.jpg',
 'wrld_small_mill': 'img/wrld-small-mill.jpg',
 'wrld_small_moll': 'img/wrld-small-moll.jpg'
