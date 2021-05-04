ccff40db936fe3579ac59385eef55e87f996784e
egax <egax@ya.ru>
2021-05-04 20:49:09 +0300

    *mosmetro svg +BFS calc route *update 2021 *fix Q bezier draw; *mosrails *fix Q

--- js/utils/metrics.js
+++
@@ -1,9 +0,0 @@
-//
-// Add metrics, analytics codes
-//
-if(!String(window.location).match(/file:|localhost/)){
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://egaxegax.github.io/static/js/metrics.js';
-  document.head.appendChild(script);
-}
